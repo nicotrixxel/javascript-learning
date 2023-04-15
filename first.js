ed151c4a66db90a1c16ab2c5f8ee7e391a5cd36e
nicotrixxel <104150729+nicotrixxel@users.noreply.github.com>
2023-04-15 17:54:00 +0200

    Create first.js

---
+++ first.js
@@ -0,0 +1,8 @@
+let name = "Nico"
+age = 16
+console.log("Hello World")
+console.log("My name is " + name + ". \nIm " + age + " years old")
+let x = 13
+let y = 28
+let result = x * y
+console.log(x + "*" + y + "="  + result)
